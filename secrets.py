9550f397ed54de6cabf5394fc56e0ee1f1b8f65f
domi <44619866+gratisCobalt@users.noreply.github.com>
2022-01-28 15:18:47 +0100

    Delete secrets.py

--- secrets.py
+++
@@ -1,2 +0,0 @@
-token = "" # paste TOKEN from https://discord.com/developers/applications/ -> (YOUR APPLICATION) -> "/bot"
-authkey = "" # paste TOKEN from https://developer.clashroyale.com/#/account 
